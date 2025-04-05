8e3f78fb390a6853e1c92a2c0d30613aad49e87f
yukito <iyggf66974@yahoo.ne.jp>
2025-04-05 16:01:17 +0900

    sidebar

--- starter/app.py
+++ starter/app.py
@@ -5,20 +5,45 @@ import json
 import pandas as pd
 import random
 
-st.title("Booking System")
 
-with st.form(key='user'):
-    user_id: int = random.randint(1, 10)
-    username: str = st.text_input("Username", max_chars=50)
 
-    data = {
-        "user_id": user_id,
-        "username": username
-    }
-    submit_button = st.form_submit_button(label='Create User')
-    if submit_button:
-        st.write("User Created")
-        st.json(data)
-        st.write("User ID:", user_id)
-        res = requests.post("http://localhost:8000/users", json=data)
-        st.json(res.json())
+
+page = st.sidebar.selectbox('Choose your page', ['users', 'rooms', 'bookings'])
+
+if page == 'users':
+    st.title("Booking System")
+
+    with st.form(key='user'):
+        user_id: int = random.randint(0, 10)
+        username: str = st.text_input("Username", max_chars=50)
+        data = {
+            # 'user_id': user_id,
+            'username': username
+        }
+        submit_button = st.form_submit_button(label='Create User')
+    
+        if submit_button:
+            st.write("User Created")
+            st.json(data)
+            st.write("User ID:", user_id)
+            res = requests.post("http://localhost:8000/users", json=data)
+            st.json(res.json())
+
+if page == 'users':
+    st.title("Booking System")
+
+    with st.form(key='user'):
+        user_id: int = random.randint(0, 10)
+        username: str = st.text_input("Username", max_chars=50)
+        data = {
+            # 'user_id': user_id,
+            'username': username
+        }
+        submit_button = st.form_submit_button(label='Create User')
+    
+        if submit_button:
+            st.write("User Created")
+            st.json(data)
+            st.write("User ID:", user_id)
+            res = requests.post("http://localhost:8000/users", json=data)
+            st.json(res.json())

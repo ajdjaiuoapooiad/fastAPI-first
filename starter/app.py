182b822ceea98adbe7f4a8a5ece990ff39150882
yukito <iyggf66974@yahoo.ne.jp>
2025-04-05 18:28:30 +0900

    get post ok

--- starter/app.py
+++ starter/app.py
@@ -3,33 +3,29 @@ import datetime
 import requests
 import json
 import pandas as pd
-import random
-
-
-
 
 page = st.sidebar.selectbox('Choose your page', ['users', 'rooms', 'bookings'])
 
 if page == 'users':
-    st.title("Booking System")
-
+    st.title('ユーザー登録画面')
     with st.form(key='user'):
-        user_id: int = random.randint(0, 10)
-        username: str = st.text_input("Username", max_chars=50)
+        # user_id: int = random.randint(0, 10)
+        username: str = st.text_input('ユーザー名', max_chars=12)
         data = {
             # 'user_id': user_id,
             'username': username
         }
-        submit_button = st.form_submit_button(label='Create User')
-    
-        if submit_button:
-            st.write("User Created")
-            st.json(data)
-            st.write("User ID:", user_id)
-            res = requests.post("http://localhost:8000/users", json=data)
-            st.json(res.json())
+        submit_button = st.form_submit_button(label='ユーザー登録')
 
-
+    if submit_button:
+        url = 'http://127.0.0.1:8000/users'
+        res = requests.post(
+            url,
+            data=json.dumps(data)
+        )
+        if res.status_code == 200:
+            st.success('ユーザー登録完了')
+        st.json(res.json())
 
 
 elif page == 'rooms':

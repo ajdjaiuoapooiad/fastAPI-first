43b07cc24a8210999f30e8c4a6a8cc7f9cf6924c
yukito <iyggf66974@yahoo.ne.jp>
2025-04-05 16:28:22 +0900

    booking

--- starter/app.py
+++ starter/app.py
@@ -36,11 +36,11 @@ elif page == 'rooms':
     st.title('会議室登録画面')
 
     with st.form(key='room'):
-        # room_id: int = random.randint(0, 10)
+        room_id: int = random.randint(0, 10)
         room_name: str = st.text_input('会議室名', max_chars=12)
         capacity: int = st.number_input('定員', step=1)
         data = {
-            # 'room_id': room_id,
+            'room_id': room_id,
             'room_name': room_name,
             'capacity': capacity
         }
@@ -58,3 +58,34 @@ elif page == 'rooms':
 
 
 
+
+elif page == 'bookings':
+    st.title('予約登録画面')
+
+    with st.form(key='booking'):
+        booking_id: int = random.randint(0, 10)
+        user_id: int = random.randint(0, 10)
+        room_id: int = random.randint(0, 10)
+        booked_num: int = random.randint(0, 10)
+        start_datetime: datetime.datetime = st.date_input('開始日時', datetime.datetime.now())
+        end_datetime: datetime.datetime = st.date_input('終了日時', datetime.datetime.now())
+        
+        data = {
+            'booking_id': booking_id,
+            'user_id': user_id,
+            'room_id': room_id,
+            'booked_num': booked_num,
+            'start_datetime': start_datetime.isoformat(),
+            'end_datetime': end_datetime.isoformat()
+        }
+        submit_button = st.form_submit_button(label='予約登録')
+
+    if submit_button:
+        url = 'http://127.0.0.1:8000/bookings'
+        res = requests.post(
+            url,
+            data=json.dumps(data)
+        )
+        if res.status_code == 200:
+            st.success('予約登録完了')
+        st.json(res.json())
